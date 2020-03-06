82acb46fef2856269473e587e42ea4b75ef9ee69
George Cooper <coopsgeorge@gmail.com>
2020-03-06 10:22:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,7 +12,6 @@ def gross_for_director(director_data)
   while director_index < nds.length
     director_name = nds[director_index][:name]
     movie_index = 0
-    result[director_name] =  0
 
   while movie_index < nds[director_index][:movies].length do
       individual_gross = 0
@@ -23,9 +22,7 @@ def gross_for_director(director_data)
     end
     director_index += 1
 end
-  result
-end
-    result
+  grand_total 
 end
 
 # Write a method that, given an NDS creates a new Hash
